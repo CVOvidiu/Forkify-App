912d05666226bb903063ec509860c4316afc8f22
CVOvidiu <cvovidiu2002@gmail.com>
2022-12-13 10:21:52 +0200

    Fetch recipe by id from Forkify API and format response

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,33 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    // Get a recipe by id
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    // Error handling : If wrong ID
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    // Recipe response format
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (e) {
+    alert(e);
+  }
+};
+showRecipe();
