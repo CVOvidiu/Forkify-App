4061d1d642d545dec851816d30ce0a164cac6478
CVOvidiu <cvovidiu2002@gmail.com>
2022-12-28 17:08:56 +0200

    Show/hide add recipe modal

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView';
 import resultsView from './views/resultsView';
 import paginationView from './views/paginationView';
 import bookmarksView from './views/bookmarksView';
+import addRecipeView from './views/addRecipeView';
 
 import 'core-js/stable'; // Polyfilling everything else
 import 'regenerator-runtime/runtime'; // Polyfilling async/await
